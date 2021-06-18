cde8b581439ecaf7c1cbd6211a708d5c4fa4c23a
Jonathan <jonathan.melosouza@gmail.com>
2021-06-18 18:37:50 -0300

    Initial commit

---
+++ Spring/src/main/java/spring/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import spring.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
